97278f22a718c548c36e069757756f2e95a7467b
3ddy98 <eaponce.vanguard@gmail.com>
2017-07-18 18:51:32 -0700

    Added easter eggs

--- interact.js
+++ interact.js
@@ -1,29 +1,27 @@
 var counter = 0;
 
-function talk(){
+function speak(){
+	alert("How did you figure that one out?! hidden2.html");
+}
 
+function whatdoesthisdo(){
 	counter = counter + 1;
-	
-	if (counter == 1){
-		var stop1 = new Audio('sounds/stop.wav')
-		stop1.play();
+	if(counter == 25){
+		alert("Well you figured it out, good job. You'll never get the next one... youfail.html")
 	}
-	else if (counter == 2){
-		var stop2 = new Audio('sounds/stop2.wav')
-		stop2.play();
+	if(counter == 15){
+		alert("Stop, it couldn't possibly be this way.");
 	}
-	else if (counter == 3){
-		var stop3 = new Audio('sounds/stop3.wav')
-		stop3.play();
+}
+function check(){
+	var date = document.getElementById('national').value
+	if(date == "2005-10-29"){
+		window.location.href="https://www.youtube.com/watch?v=J---aiyznGQ";
+		alert("Working");
+		return false;
 	}
-	else if (counter == 4){
-		var stop4 = new Audio('sounds/stop4.wav')
-		stop4.play();
+	else{
+		alert("Nope.")
 	}
-	else if (counter == 5){
-		var stop5 = new Audio('sounds/stop5.wav');
-		stop5.play();
-		document.body.innerHTML='<h1>Fuck Off</h1>';
-	}
-
+	return false;
 }
