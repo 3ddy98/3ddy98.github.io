4afd337870ed7fa7e408483f0cb47ddac0962908
3ddy98 <eaponce.vanguard@gmail.com>
2018-10-25 18:05:38 -0700

    [+] Fixed some margin issues and corrected background image scaling

---
+++ scripts/divwidth.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#blurred-background").css({
+    'width':($("#intro-text").width()+'px')
+  });
+  $("#blurred-background").css({
+    'height':($("#intro-text").height()+'px')
+  });
+});
